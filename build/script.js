850ced67a6f987be55d3f7dfd941850c31d69f66
Ilya <ilshishkin@gmail.com>
2019-07-23 23:25:51 +0300

    обработка события DomContentLoaded

--- build/script.js
+++ build/script.js
@@ -1,29 +1,31 @@
-document.body.addEventListener('click', function (event) {
-  let target = event.target;
+document.addEventListener('DOMContentLoaded', function () {
+  document.body.addEventListener('click', function (event) {
+    let target = event.target;
 
-  while (target !== this) {
-    if (target.classList.contains('onoffswitch')) {
-      const rootNode = document.querySelector('.theme');
+    while (target !== this) {
+      if (target.classList.contains('onoffswitch')) {
+        const rootNode = document.querySelector('.theme');
 
-      if (rootNode.classList.contains('theme_color_project-default')) {
-        rootNode.classList.remove('theme_color_project-default');
-        rootNode.classList.add('theme_color_project-inverse');
-      } else if (rootNode.classList.contains('theme_color_project-inverse')) {
-        rootNode.classList.remove('theme_color_project-inverse');
-        rootNode.classList.add('theme_color_project-default');
-      }
-    }
-
-    if (target.classList.contains('e-accordion') && !target.classList.contains('e-accordion__more')) {
-      const accordionMore = target.querySelector('.e-accordion__more');
-
-      if (accordionMore) {
-        accordionMore.classList.toggle('e-accordion__more_view_default');
+        if (rootNode.classList.contains('theme_color_project-default')) {
+          rootNode.classList.remove('theme_color_project-default');
+          rootNode.classList.add('theme_color_project-inverse');
+        } else if (rootNode.classList.contains('theme_color_project-inverse')) {
+          rootNode.classList.remove('theme_color_project-inverse');
+          rootNode.classList.add('theme_color_project-default');
+        }
       }
 
-      return;
-    }
+      if (target.classList.contains('e-accordion') && !target.classList.contains('e-accordion__more')) {
+        const accordionMore = target.querySelector('.e-accordion__more');
 
-    target = target.parentNode;
-  }
+        if (accordionMore) {
+          accordionMore.classList.toggle('e-accordion__more_view_default');
+        }
+
+        return;
+      }
+
+      target = target.parentNode;
+    }
+  });
 });
